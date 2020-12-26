69ecd6c843cc4014d862e5f904ce0fae71c1c546
Mustafa EREN <54962045+MustafaErenn@users.noreply.github.com>
2020-12-26 16:05:35 +0300

    Delete pushStackHeaders.h

--- headers/pushStackHeaders.h
+++
@@ -1,8 +0,0 @@
-void pushStack(STACK* stack,char giris)
-{
-    NODE* node1 = (NODE*)malloc(sizeof(NODE));
-    node1->data = giris;
-    node1->next = stack->top;
-    stack->top = node1;
-    stack->count++;
-}
